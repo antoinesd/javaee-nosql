7575ac640f2d99cc918dc2c87df8f1f323a0d2a6
Antoine Sabot-Durand <antoine@sabot-durand.net>
2014-11-28 19:21:21 +0100

    Corrected typo in module name and folder corrected bean discovery mode

--- mongodb/src/main/java/org/jboss/javaee/mongodb/AnyLiteral.java
+++ mongodb/src/main/java/org/jboss/javaee/mongodb/AnyLiteral.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.jboss.javaee.mongodb;
 
 import javax.enterprise.inject.Any;
